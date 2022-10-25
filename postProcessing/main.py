e8b7627b1c517b4aa7a7bec62118576b171b442c
tama3rdGen <tamatomo.21@gmail.com>
2022-10-25 12:08:34 -0500

    Minor update

--- postProcessing/main.py
+++ postProcessing/main.py
@@ -9,24 +9,29 @@ from scipy.optimize import minimize
 
 con=conditions.conditions
 
-con.teq=0.5e-6     # equilibliumed time [s]
-con.tEND=0.5e-6
-con.tcut=1e-13       # cut residence time [s]
-con.Nmax=40         # Max number of sticking vapors
+con.teq=0.2e-6     # equilibliumed time [s]
+con.tEND=5e-6
+con.tcut=1e-9       # cut residence time [s]
+con.Nmax=20         # Max number of sticking vapors
 con.dt_post=1e-11   # dt in analysis, dt_post [s]
 con.startTime=2e-9
 con.endTime=5e-9
 
-con.directory="/home/tama3rdgen/vaporUptake/angioII+2/testRun/"
-con.inputDirectory="/home/tama3rdgen/vaporUptake/angioII+2/input/"
-#con.directory="../../../nucleation/valine/"
-con.I=1
+#con.directory="/home/tama3rdgen/vaporUptake/angioII+2/"
+con.directory="/home/tama3rdgen/test2/valine/500Pa_MeOH/"
+con.I=0
 con.figOutput=0
+con.pv=500
+con.setMasses(con,32e-3,117.15e-3)
+con.calcParams(con)
+
+clu=cluster.cluster(con)
+clu.Upot()
+clu.vapor()
 
 #clu=cluster.cluster(con)
 #clu.compute()
 #diff=diffusionCoeff.diffusionCoeff(con)
 #diff.compute()
-con.replica=[2]
-stk=stickPosition.stickPosition(con)
-stk.compute()
+#stk=stickPosition.stickPosition(con)
+#stk.compute()
--- postProcessing/plot.py
+++ postProcessing/plot.py
@@ -7,9 +7,10 @@ from scipy.optimize import minimize
 
 class plot:
 
-    labelSize=10
-    titleSize=12
-    lineWidth=0.8
+    labelSize=12
+    titleSize=15
+    lineWidth=1
+    fs=(5,5)
 
     #-----------------------------------------------------------------------------#
     def pltNormal(self):
@@ -29,7 +30,11 @@ class plot:
     				## Reading energy file(s)
     #-----------------------------------------------------------------------------#
 
-    def plotEnergies(self,U,teq,tEND,figIndex):
+    def plotEnergies(self,U,teq,tEND,figOutput):
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=self.fs)
+        self.axNormal(axs)
+
         #K=np.loadtxt("K_1.dat")
         labels=["Uion","Ugas","Uvap","Ugi","Ugg","Uvg","Uvi","Uvv"]
         display=[1,0,0,0,0,0,1,1]
@@ -39,94 +44,114 @@ class plot:
         Xmax=int(t_tot/200e-9)*200
         Xmax*=1.4
 
-        #axs.flat[0].set_xlim([0,Xmax])
-        self.axs.flat[figIndex].set_title("(a) Potential Energies",loc='left',fontsize=self.titleSize)
-        self.axs.flat[figIndex].set_xlabel("Time [ns]",fontsize=self.labelSize)
-        self.axs.flat[figIndex].set_ylabel("Energy [kcal/mol]",fontsize=self.labelSize)
+        #axs.set_xlim([0,Xmax])
+        axs.set_xlabel("Time [ns]",fontsize=self.labelSize)
+        axs.set_ylabel("Energy [kcal/mol]",fontsize=self.labelSize)
         for i in np.arange(np.size(U[0])-1):
             if(display[i]):
-        	       self.axs.flat[figIndex].plot(U.T[0]*1e-6,U.T[i+1],label=labels[i],linewidth=self.lineWidth,color=colors[i])
-        self.axs.flat[figIndex].plot(U.T[0]*1e-6,np.sum(U.T[1:],axis=0),label="Total",linewidth=self.lineWidth,color="black")
-        self.axs.flat[figIndex].axvline(x = teq*1e9, ls='--', color = 'black',linewidth=self.lineWidth)
-        self.axs.flat[figIndex].legend()
+        	       axs.plot(U.T[0]*1e-6,U.T[i+1],label=labels[i],linewidth=self.lineWidth,color=colors[i])
+        axs.plot(U.T[0]*1e-6,np.sum(U.T[1:],axis=0),label="Total",linewidth=self.lineWidth,color="black")
+        axs.axvline(x = teq*1e9, ls='--', color = 'black',linewidth=self.lineWidth)
+        axs.legend()
+        fig.tight_layout()
+        if(figOutput):
+            plt.savefig(fileName, dpi=1000)
+        plt.show()
 
     				## Reading vapor in out time files
     #-----------------------------------------------------------------------------#
-    def plotNvap(self,times,teq,Nstick):
-        self.axs.flat[1].set_title("(b) MeOHs in effective domain",loc='left',fontsize=self.titleSize)
-        self.axs.flat[1].set_xlabel("Time [ns]",fontsize=self.labelSize)
-        self.axs.flat[1].set_ylabel("$\it {N}$$_ {vap}$ [-]",fontsize=self.labelSize)
-        self.axs.flat[1].axvline(x = teq*1e9, color = 'black', ls="--",linewidth=self.lineWidth)
-        self.axs.flat[1].scatter(times*1e9,Nstick,color="black",s=20)
+    def plotNvap(self,times,teq,Nstick,figOutput):
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=(5,5))
+        self.axNormal(axs)
+        axs.set_xlabel("Time [ns]",fontsize=self.labelSize)
+        axs.set_ylabel("$\it {N}$$_ {vap}$ [-]",fontsize=self.labelSize)
+        axs.axvline(x = teq*1e9, color = 'black', ls="--",linewidth=self.lineWidth)
+        axs.scatter(times*1e9,Nstick,color="black",s=20)
+        fig.tight_layout()
+        if(figOutput):
+            plt.savefig(fileName, dpi=1000)
+        plt.show()
 
     #-----------------------------------------------------------------------------#
-    def plotStickTimeDist(self,ts,tth):
+    def plotStickTimeDist(self,ts,tth,figOutput):
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=(5,5))
+        self.axNormal(axs)
         negs=np.where(ts<=0)
-        self.axs.flat[2].set_title("(c) Vapor sticking time distribution",loc='left',fontsize=self.titleSize)
-        self.axs.flat[2].set_xlabel("Logarithm of time [-]",fontsize=self.labelSize)
-        self.axs.flat[2].set_ylabel("Number of event [-]",fontsize=self.labelSize)
-        self.axs.flat[2].set_yscale("log")
-        self.axs.flat[2].hist(np.log10(np.delete(ts,negs)),alpha=0.3,bins=50,label="$\it {t}$$_{sim}$",color="blue")
-        self.axs.flat[2].hist(np.log10(np.delete(tth,negs)),alpha=0.3,bins=30,label="$\it {t}$$_ {sim}$-$\it t$$_ {th}$",color="black")
-        #axs.flat[2].axvline(x = np.log10(tcut), color = 'black', ls="--",linewidth=lineWidth)
-        self.axs.flat[2].legend()
+        axs.set_xlabel("Logarithm of time [-]",fontsize=self.labelSize)
+        axs.set_ylabel("Number of event [-]",fontsize=self.labelSize)
+        axs.set_yscale("log")
+        axs.hist(np.log10(np.delete(ts,negs)),alpha=0.3,bins=50,label="$\it {t}$$_{sim}$",color="blue")
+        axs.hist(np.log10(np.delete(tth,negs)),alpha=0.3,bins=30,label="$\it {t}$$_ {sim}$-$\it t$$_ {th}$",color="black")
+        #axs.axvline(x = np.log10(tcut), color = 'black', ls="--",linewidth=lineWidth)
+        axs.legend()
+        fig.tight_layout()
+        if(figOutput):
+            plt.savefig(fileName, dpi=1000)
+        plt.show()
 
     #-----------------------------------------------------------------------------#
-    def plotStickVaporDist(self,nv,ppoi,psim,Nbase):
-        self.axs.flat[3].set_title("(d) Sticking vapor distribution",loc='left',fontsize=self.titleSize)
-        self.axs.flat[3].set_title("")
-        self.axs.flat[3].set_xlabel("Number of sticking vapor",fontsize=self.labelSize)
-        self.axs.flat[3].set_ylabel("Frequency [-]",fontsize=self.labelSize)
-        self.axs.flat[3].set_yscale("log")
-        self.axs.flat[3].set_ylim([1e-5,1])
-        self.axs.flat[3].scatter(nv,ppoi,label="Poisson",s=20,color="black")
-        self.axs.flat[3].scatter(nv,psim,label="Simulation",s=20,marker="v",color="blue")
-        self.axs.flat[3].scatter(nv-Nbase,psim,label="Simulation("+str(int(-Nbase))+")",s=20,marker="^",color="cyan")
-        self.axs.flat[3].legend()
+    def plotStickVaporDist(self,nv,ppoi,psim,Nbase,figOutput):
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=(5,5))
+        self.axNormal(axs)
+        axs.set_xlabel("Number of sticking vapor",fontsize=self.labelSize)
+        axs.set_ylabel("Frequency [-]",fontsize=self.labelSize)
+        axs.set_yscale("log")
+        axs.set_ylim([1e-5,1])
+        axs.scatter(nv,ppoi,label="Poisson",color="black")
+        axs.scatter(nv,psim,label="Simulation",color="blue")
+        #axs.scatter(nv-Nbase,psim,label="Simulation("+str(int(-Nbase))+")",marker="^",color="cyan")
+        axs.legend()
+        fig.tight_layout()
+        if(figOutput):
+            plt.savefig(fileName, dpi=1000)
+        plt.show()
 
     #-----------------------------------------------------------------------------#
-    def plotMSDVAF(self,MSDVAF,diffusionData):
-        self.axs.flat[0].set_title("(e) Normalized MSD",loc='left',fontsize=self.titleSize)
-        self.axs.flat[0].set_xlabel("Time [ns]",fontsize=self.labelSize)
-        self.axs.flat[0].set_ylabel("Normalized MSD [-]",fontsize=self.labelSize)
-        self.axs.flat[0].scatter(MSDVAF.T[0],MSDVAF.T[1]/np.max(MSDVAF.T[1]),color="black",s=10)
-        self.axs.flat[0].text(1, 0.8, "K = "+'{:.2f}'.format(diffusionData[3]*diffusionData[5])+" cm$^ 2$/Vs", fontsize = 10)
-        self.axs.flat[1].set_title("(f) Normalized VAF",loc='left',fontsize=self.titleSize)
-        self.axs.flat[1].set_xlabel("Time [ns]",fontsize=self.labelSize)
-        self.axs.flat[1].set_ylabel("Normalized VAF [-]",fontsize=self.labelSize)
-        self.axs.flat[1].scatter(MSDVAF.T[0],MSDVAF.T[2]/MSDVAF.T[2][0],color="black",s=10)
-        self.axs.flat[1].text(2, 0.8, "K = "+'{:.2f}'.format(diffusionData[4]*diffusionData[5])+" cm$^ 2$/Vs", fontsize = 10)
+    def plotMSDVAF(self,MSDVAF,diffusionData,figOutput):
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=(5,5))
+        self.axNormal(axs)
+        axs.set_title("(e) Normalized MSD",loc='left',fontsize=self.titleSize)
+        axs.set_xlabel("Time [ns]",fontsize=self.labelSize)
+        axs.set_ylabel("Normalized MSD [-]",fontsize=self.labelSize)
+        axs.scatter(MSDVAF.T[0],MSDVAF.T[1]/np.max(MSDVAF.T[1]),color="black",s=10)
+        axs.text(1, 0.8, "K = "+'{:.2f}'.format(diffusionData[3]*diffusionData[5])+" cm$^ 2$/Vs", fontsize = 10)
+        axs.set_title("(f) Normalized VAF",loc='left',fontsize=self.titleSize)
+        axs.set_xlabel("Time [ns]",fontsize=self.labelSize)
+        axs.set_ylabel("Normalized VAF [-]",fontsize=self.labelSize)
+        axs.scatter(MSDVAF.T[0],MSDVAF.T[2]/MSDVAF.T[2][0],color="black",s=10)
+        axs.text(2, 0.8, "K = "+'{:.2f}'.format(diffusionData[4]*diffusionData[5])+" cm$^ 2$/Vs", fontsize = 10)
+        fig.tight_layout()
+        if(figOutput):
+            plt.savefig(fileName, dpi=1000)
+        plt.show()
 
     #-----------------------------------------------------------------------------#
     def plotShow(self,figOutput,fileName):
-        self.fig.tight_layout()
+        fig.tight_layout()
         if(figOutput):
             plt.savefig(fileName, dpi=1000)
         plt.show()
 
     def plotMobilityShift(self,datas,directory):
-        #axs.flat[0].set_xlim([0,Xmax])
-        self.axs.flat[0].set_title("(a) Inverse of mobility shift",loc='left',fontsize=self.titleSize)
-        self.axs.flat[0].set_xlabel("Vapor pressure [Pa]",fontsize=self.labelSize)
-        self.axs.flat[0].set_ylabel(r"Mobility [cm$^2$/Vs]",fontsize=self.labelSize)
+        self.pltNormal()
+        fig, axs = plt.subplots(1,1,figsize=(5,5))
+        self.axNormal(axs)
+        #axs.set_xlim([0,Xmax])
+        axs.set_title("(a) Inverse of mobility shift",loc='left',fontsize=self.titleSize)
+        axs.set_xlabel("Vapor pressure [Pa]",fontsize=self.labelSize)
+        axs.set_ylabel(r"Mobility [cm$^2$/Vs]",fontsize=self.labelSize)
         for data in datas:
-            self.axs.flat[0].scatter(data[0]*10,(data[4]*data[6])**-1,color = "black")
+            self.scatter(data[0]*10,(data[4]*data[6])**-1,color = "black")
 
-        self.axs.flat[1].set_title("(b) Mobility shift",loc='left',fontsize=self.titleSize)
-        self.axs.flat[1].set_xlabel("Vapor pressure [Pa]",fontsize=self.labelSize)
-        self.axs.flat[1].set_ylabel(r"Normalized mobility, $Z_p/Z_{p,0}$ [-]",fontsize=self.labelSize)
+        axs.set_title("(b) Mobility shift",loc='left',fontsize=self.titleSize)
+        axs.set_xlabel("Vapor pressure [Pa]",fontsize=self.labelSize)
+        axs.set_ylabel(r"Normalized mobility, $Z_p/Z_{p,0}$ [-]",fontsize=self.labelSize)
         for data in datas:
-            self.axs.flat[1].scatter(data[0]*10,(data[4]/datas[0][4]),color = "black")
+            axs.scatter(data[0]*10,(data[4]/datas[0][4]),color = "black")
 
         plt.savefig(str(directory)+"diffusionSummary.png", dpi=1000)
-        plt.show()
-
-    def plotStickLocation(self,label,dist,aminoNames,distAmino,directory):
-        self.axs.flat[0].set_xlabel("Hidrogen location ID",fontsize=12)
-        self.axs.flat[0].set_ylabel("Probability [%]",fontsize=12)
-        self.axs.flat[0].bar(label,dist)
-        self.axs.flat[1].set_xlabel("Amino acides",fontsize=12)
-        self.axs.flat[1].set_ylabel("Probability [%]",fontsize=12)
-        self.axs.flat[1].bar(aminoNames,distAmino)
-        plt.savefig(str(directory)+"stickLocationDist.png", dpi=1000)
         plt.show()
